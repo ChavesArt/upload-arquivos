f7547713c836cce4e826a656ca3f8c5f1a738259
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-06-07 15:01:53 -0300

    nada importante

--- index.php
+++ index.php
@@ -13,11 +13,11 @@ else{
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>upload de arquivos</title>
 </head>
 <body>
     <form action="upload.php" method="post" enctype="multipart/form-data">
